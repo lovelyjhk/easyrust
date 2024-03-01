97876c676dfb4398271605c85fed1d0dfbd00ece
lovelyjhk <lovelyjhk@gmail.com>
2024-03-01 13:32:06 +0900

    Initial commit

---
+++ target/debug/deps/easyrustv1-3f6c0856f3f11e5a.d
@@ -0,0 +1,5 @@
+C:\Users\lovel\OneDrive\Documents\GitHub\easyrustv1\target\debug\deps\libeasyrustv1-3f6c0856f3f11e5a.rmeta: src\main.rs
+
+C:\Users\lovel\OneDrive\Documents\GitHub\easyrustv1\target\debug\deps\easyrustv1-3f6c0856f3f11e5a.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/easyrustv1.d
@@ -0,0 +1,1 @@
+C:\Users\lovel\OneDrive\Documents\GitHub\easyrustv1\target\debug\easyrustv1.exe: C:\Users\lovel\OneDrive\Documents\GitHub\easyrustv1\src\main.rs

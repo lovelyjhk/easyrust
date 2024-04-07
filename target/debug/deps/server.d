6218d984ff9ead48e8db7cf7b619a7afab41b661
lovelyjhk <lovelyjhk@gmail.com>
2024-04-07 22:43:51 +0900

    Initial commit

---
+++ target/debug/deps/server.d
@@ -0,0 +1,5 @@
+C:\Users\lovel\OneDrive\Documents\GitHub\easyrustv1\target\debug\deps\server.exe: platinum\var_4_9\server\src\main.rs
+
+C:\Users\lovel\OneDrive\Documents\GitHub\easyrustv1\target\debug\deps\server.d: platinum\var_4_9\server\src\main.rs
+
+platinum\var_4_9\server\src\main.rs:
---
+++ target/debug/server.d
@@ -0,0 +1,1 @@
+C:\Users\lovel\OneDrive\Documents\GitHub\easyrustv1\target\debug\server.exe: C:\Users\lovel\OneDrive\Documents\GitHub\easyrustv1\platinum\var_4_9\server\src\main.rs
